8423caa6ffe30fa5a97890c3e559552d8f8f48ac
skhan <skhan@staff.compass.edu.au>
2020-10-13 17:49:32 +1100

    individual news item

---
+++ client/src/components/NewsList/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+
+function NewsForm({news}) {
+    const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+    <CardContent>
+      <Typography className={classes.title} color="textSecondary" gutterBottom>
+        {news.webTitle}
+      </Typography>
+      <Typography component="p">
+        {news.webPublicationDate}
+      </Typography>
+        <input type='checkbox'/>
+    </CardContent>
+    <CardActions>
+      <Button size="small"><a href={news.webUrl}>Learn More</a></Button>
+    </CardActions>
+  </Card>
+  );
+}
+
+export default NewsForm;
